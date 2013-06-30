71d8e839dea30dab40f00ef74c9b56377d68ce0e
Etienne de la Broise <etienne-delabroise@cifacom.com>
2013-06-30 15:09:49 +0200

    rails folder creation

---
+++ Library/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Library::Application.config.secret_token = '336fe83191c02a9097db3f49f8bfa998032853c9cab1e36cfee794a7f9e13e7ae4b89f3a33545dcc7cdf735caf4b70da76c05f71b4ec7cad62ac2a51b19b5277'
