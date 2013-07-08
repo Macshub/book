ada13005c357198ba1c1c3947609103235c4f6e3
Etienne de la Broise <etienne-delabroise@cifacom.com>
2013-07-08 22:14:49 +0200

    Finishing books#search action

--- Library/app/controllers/books_controller.rb
+++ Library/app/controllers/books_controller.rb
@@ -1,28 +1,54 @@
 class BooksController < ApplicationController
+  def create
+    # TODO :: Fill this action
+  end
   def index
     @books = Book.all
   end
   def new
   end
   def search
+    # Start GoodReads API request
     url = Nokogiri::XML(open("http://www.goodreads.com/book/isbn?isbn="+params[:book][:isbn]+"&key=mGuETvHmUq2AJGNpuyPhDw&format=xml")).xpath("//book").first
 
-    @hash_book = {
+    if url.xpath("publication_year").text == '' || url.xpath("publication_month").text == '' || url.xpath("publication_day").text == ''
+      publication = nil
+    else
+      publication = Date.parse(url.xpath("publication_year").text + '-' + url.xpath("publication_month").text + '-' + url.xpath("publication_day").text)
+    end
+    if url.xpath("original_publication_year").text == '' || url.xpath("original_publication_month").text == '' || url.xpath("original_publication_day").text == ''
+      original_publication = nil
+    else
+      original_publication = Date.parse(url.xpath("original_publication_year").text + '-' + url.xpath("original_publication_month").text + '-' + url.xpath("original_publication_day").text)
+    end
+    api_result = {
+      author_name: url.xpath("authors/author/name").text,
+      cover: url.xpath("image_url").text,
+      description: url.xpath("description").text,
       isbn: url.xpath("isbn").text,
       isbn13: url.xpath("isbn13").text,
-      title: url.xpath("title").text,
-      image_url: url.xpath("image_url").text,
-      publication_year: url.xpath("publication_year").text,
-      publication_month: url.xpath("publication_month").text,
-      publication_day: url.xpath("publication_day").text,
-      original_publication_day: url.xpath("work/original_publication_day").text,
-      original_publication_month: url.xpath("work/original_publication_month").text,
-      original_publication_year: url.xpath("work/original_publication_year").text,
+      original_publication: original_publication,
+      pages: url.xpath("num_pages").text,
+      publication: publication,
       publisher: url.xpath("publisher").text,
-      description: url.xpath("description").text,
-      author_name: url.xpath("authors/author/name").text,
-      average_rating: url.xpath("average_rating").text,
-      num_pages: url.xpath("num_pages").text
+      rating: url.xpath("average_rating").text,
+      title: url.xpath("title").text
     }
+    # End GoodReads API request
+
+    book_hash = api_result
+    @book = Book.new(
+      author_name: book_hash[:author_name],
+      cover: book_hash[:cover],
+      description: book_hash[:description],
+      isbn: book_hash[:isbn],
+      isbn13: book_hash[:isbn13],
+      original_publication: book_hash[:original_publication],
+      pages: book_hash[:pages],
+      publication: book_hash[:publication],
+      publisher: book_hash[:publisher],
+      rating: book_hash[:rating],
+      title: book_hash[:title]
+    )
   end
 end
--- Library/app/models/book.rb
+++ Library/app/models/book.rb
@@ -9,9 +9,9 @@ class Book < ActiveRecord::Base
   end
 
   validates_associated :user
-  validates :author_name, :cover, :description, :isbn, :isbn13, :lent, :original_publication, :pages, :publication, :publisher, :rating, :status, :title, presence: true
-  validates :isbn, length: { is: 9 }
-  validates :isbn13, length: { is: 13 }
-  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  validates :author_name, :cover, :description, :isbn, :isbn13, :lent, :original_publication, :pages, :publication, :publisher, :rating, :status, :title, presence: true, allow_nil: true
+  validates :isbn, length: { is: 9 }, allow_nil: true
+  validates :isbn13, length: { is: 13 }, allow_nil: true
+  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }, allow_nil: true
   validates :status, inclusion: { in: %w('model.book.status.to_buy', 'model.book.status.to_read', 'model.book.status.already_read') }
 end
